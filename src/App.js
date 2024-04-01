eb7f812f60c2994f67239eb9010c1ec0325fc5b7
Stanley Andrade <45856878+StanleyAndrade@users.noreply.github.com>
2024-04-01 14:37:07 -0300

    ain

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Loja from "./assets/js/loja";
+import './assets/css/loja.css'
 
 const App = () => {
   return (
