dcd61937bcf2fd3794f758d732f910a6a4f3101c
Stanley Andrade <45856878+StanleyAndrade@users.noreply.github.com>
2024-05-14 01:20:37 -0300

    ain

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Loja from "./assets/js/loja";
-import './assets/css/loja.css'
+import Home from "./assets/js/home";
+import './assets/css/home.css'
 
 const App = () => {
   return (
     <Router>
       <Routes>
         {/* Rota para a página de perfil do usuário */}
-        <Route path="/:username" element={<Loja/>} />
+        <Route path="/:username" element={<Home/>} />
         {/* Outras rotas da sua aplicação */}
       </Routes>
     </Router>
